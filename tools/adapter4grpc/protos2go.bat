f0898d193e2d7f67a0947bbc6e9c45483a6cf3ea
wup364 <wup364@outlook.com>
2023-07-23 19:17:34 +0800

    删除无用测试文件

--- tools/adapter4grpc/protos2go.bat
+++
@@ -1,4 +0,0 @@
-call go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-call go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
-@REM call protoc -I ./protos/ ./protos/*.proto --go_out=plugins=grpc:.
-call protoc --go_out=. --go_opt=paths=import --go-grpc_out=require_unimplemented_servers=false:. --go-grpc_opt=paths=import ./protos/*.proto
--- tools/adapter4grpc/protos2js.bat
+++
@@ -1,8 +0,0 @@
-SET OUT_DIR=./sdk/js/libs
-
-call npm install request -g
-call npm config set unsafe-perm true
-call npm install protoc-gen-grpc -g
-@REM  build
-call protoc-gen-grpc --js_out=import_style=commonjs,binary:%OUT_DIR% --grpc_out=grpc_js:%OUT_DIR% --proto_path ./protos ./protos/*.proto
-call protoc-gen-grpc-ts --ts_out=grpc_js:%OUT_DIR% --proto_path ./protos ./protos/*.proto
